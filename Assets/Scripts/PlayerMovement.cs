386541b37b1b0acb72c9c4cb85f75ba040e9b906
RamiAlmuhana <ramealmuhana@hotmail.com>
2024-12-14 17:38:19 +0100

    1. animatie aangemaakt voor de speler als die naar links en rechts gaat en als hij springt 2. speler draait nu ook naar link en rechts

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,4 +1,5 @@
 using System;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
@@ -6,19 +7,52 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
     private float speed;
     private Rigidbody2D body;
+    private Animator anim;
+    private bool grounded;
 
     private void Awake()
     {
         body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     private void Update()
     {
-        body.linearVelocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
+        float horizontalInput = Input.GetAxis("Horizontal");
+        
+        body.linearVelocity = new Vector2(horizontalInput * speed, body.velocity.y);
 
-        if (Input.GetKey(KeyCode.Space))
+        //flip player when moving left-right
+        if (horizontalInput > 0.01f)
         {
-            body.linearVelocity = new Vector2(body.velocity.x, speed);
+            transform.localScale = Vector3.one;
+        } else if (horizontalInput < -0.01f)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        
+        if (Input.GetKey(KeyCode.Space) && grounded)
+        {
+            Jump();
+        }
+        
+        anim.SetBool("run", horizontalInput != 0);
+        anim.SetBool("grounded", grounded);
+        
+    }
+
+    private void Jump()
+    {
+        body.linearVelocity = new Vector2(body.velocity.x, speed);
+        anim.SetTrigger("jump");
+        grounded = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            grounded = true;
         }
     }
 }
