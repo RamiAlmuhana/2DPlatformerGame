dfae95700f828e209dec333bf863e61ae70f3808
RamiAlmuhana <ramealmuhana@hotmail.com>
2024-12-16 18:55:25 +0100

    1. Main Menu aangemaakt 2. Level 3 helemaal afgemaakt daarin heb ik spikes toegevoegd die omhoog en omlaag gaan met een delay dat als die naar boven gaat dat die stopt voor zoveel seconden

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -11,7 +11,10 @@ public class CameraController : MonoBehaviour
 
     private void Update()
     {
-        Vector3 targetPosition = target.position + offset;
-        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+        if (target != null)
+        {
+            Vector3 targetPosition = target.position + offset;
+            transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+        }
     }
 }
